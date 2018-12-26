fd7ba562c60c6de7c484c7b9ee3ebb318cca884b
xyw <1206343412@qq.com>
2018-12-26 13:46:58 +0800

    视图1

--- views.py
+++ views.py
@@ -1,1 +1,23 @@
-this is a code test
+
+# python3 抓取bing今日美图的所有背景图片
+import requests
+from pyquery import PyQuery as pq
+ 
+def download(p):
+    url = "http://bing.plmeizi.com/show/"+str(p)
+    res = requests.get(url).text
+    doc = pq(res)
+    img_url = doc.find('#picurl').attr("href")
+    print(i, img_url)
+    if img_url:
+        right = img_url.rindex('/')
+        right1 = img_url.rindex('Z')
+        name = img_url[right + 1:right1 - 1] + ".jpg"     # 修改图片名,去掉多余字段.
+        img = requests.get(img_url).content
+        with open(".\\Bing_Photos\\" + name, "wb") as f:     # 手动在程序同目录下建一个文件夹Bing_Photos,用于放置图片.
+            f.write(img)
+ 
+for i in range(890, 820, -1):     # 设置爬取天数，800对应日期20180805，依次增减即可。
+    download(i)
+ 
+
